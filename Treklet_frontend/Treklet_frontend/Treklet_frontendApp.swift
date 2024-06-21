4e2e0187e89a69b1aad3bba861c9f1d80d2945f5
Goldguyer <160653890+Goldguyer@users.noreply.github.com>
2024-06-21 17:53:14 -0400

    frontend and backend projects setup

---
+++ Treklet_frontend/Treklet_frontend/Treklet_frontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  Treklet_frontendApp.swift
+//  Treklet_frontend
+//
+//  Created by Max Goldstein on 6/21/24.
+//
+
+import SwiftUI
+
+@main
+struct Treklet_frontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
